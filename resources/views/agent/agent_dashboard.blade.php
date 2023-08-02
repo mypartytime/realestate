3e1f6e19910462ae4fc95b362e33781a3b6cee9c
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-08-02 20:05:42 +0700

    004 Breez multilogin final

---
+++ resources/views/agent/agent_dashboard.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agent dashboard</title>
+</head>
+<body>
+
+<h3>เอเย่นต์ แดชบอร์ด</h3>
+    
+</body>
+</html>

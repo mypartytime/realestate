ba796e4c4c3ce3803e50b37f691c5d0dbc9f772b
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-08-09 20:11:12 +0700

    011 frontend template setup

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,13 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// frontend all
+Route::get('/', [UserController::class, 'Index']);
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');

3afb027d740238f0f245bdb0dcf555dde12deb93
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-08-09 14:18:16 +0700

    008 Admin image update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -24,5 +25,37 @@ class AdminController extends Controller
 
     public function AdminLogin(){
         return view('admin.admin_login');
-    }
+    } // end method
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
+
+    } // end method
+
+    public function AdminProfileStore(Request $request){
+       
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        //store image section
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        return redirect()->back();
+
+    } // end method
 }

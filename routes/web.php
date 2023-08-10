d88a88c4d0a3c5fb53362337634da88e218995c1
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-08-10 20:10:19 +0700

    021 Amenities Crud p-3 Store Update Delete

--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,10 @@ Route::middleware(['auth','role:admin'])->group(function(){
 
         Route::get('/all/amenitie', 'AllAmenitie')->name('all.amenitie'); 
         Route::get('/add/amenitie', 'AddAmenitie')->name('add.amenitie');
+        Route::post('/store/amenitie', 'StoreAmenitie')->name('store.amenitie'); 
+        Route::get('/edit/amenitie/{id}', 'EditAmenitie')->name('edit.amenitie');
+        Route::post('/update/amenitie', 'UpdateAmenitie')->name('update.amenitie');
+        Route::get('/delete/amenitie/{id}', 'DeleteAmenitie')->name('delete.amenitie'); 
     
 
     });

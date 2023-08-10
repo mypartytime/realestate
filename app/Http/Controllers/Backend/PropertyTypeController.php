0b3b4668c636f6b9697b51d2f3e9aca5a5a99be0
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-08-10 17:07:14 +0700

    020 Property Type Crud p-3 Edit

--- app/Http/Controllers/Backend/PropertyTypeController.php
+++ app/Http/Controllers/Backend/PropertyTypeController.php
@@ -16,4 +16,59 @@ class PropertyTypeController extends Controller
         return view('backend.type.all_type',compact('types'));
 
     } // End Method 
+
+    public function AddType(){
+
+        return view('backend.type.add_type');
+
+    } // End Method 
+
+    public function StoreType(Request $request){
+
+        // Validation 
+        $request->validate([
+            'type_name' => 'required|unique:property_types|max:200', // ห้ามใส่ข้อมูลซ้ำระบบจะเกิด error เตือนทันที
+            'type_icon' => 'required'
+
+        ]);
+
+        PropertyType::insert([
+            'type_name' => $request->type_name,
+            'type_icon' => $request->type_icon
+        ]);
+
+        $notification = array(
+            'message' => 'Type Create Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.type')->with($notification); 
+
+    } // End Method 
+
+    public function EditType($id){
+
+        $types = PropertyType::findOrFail($id);
+        return view('backend.type.edit_type',compact('types'));
+
+    } // End Method 
+
+    public function UpdateType(Request $request){
+
+       $pid = $request->id;
+
+       PropertyType::findOrFail($pid)->update([
+        'type_name' => $request->type_name,
+        'type_icon' => $request->type_icon,
+    ]);
+
+
+        $notification = array(
+            'message' => 'Type Update Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.type')->with($notification); 
+
+    } // End Method 
 }
--- resources/views/backend/type/all_type.blade.php
+++ resources/views/backend/type/all_type.blade.php
@@ -5,7 +5,7 @@
 
 				<nav class="page-breadcrumb">
 					<ol class="breadcrumb">
-	  <a href="" class="btn btn-inverse-info"> Add Property Type  </a>
+	  <a href="{{route('add.type')}}" class="btn btn-inverse-info"> Add Property Type  </a>
 					</ol>
 				</nav>
 
@@ -32,7 +32,7 @@
                         <td>{{ $item->type_name }}</td>
                         <td>{{ $item->type_icon }}</td>
                         <td>
-       <a href="" class="btn btn-inverse-warning"> Edit </a>
+       <a href="{{ route('edit.type',$item->id) }}" class="btn btn-inverse-warning"> Edit </a>
        <a href="" class="btn btn-inverse-danger"> Delete  </a>
                         </td> 
                       </tr>

4e7d2dbe594ca705a6d9b543d0466eeedf380647
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-08-24 18:17:18 +0700

    037 Agent login and register

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,13 @@ Route::middleware(['auth','role:agent'])->group(function(){
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login')->middleware(RedirectIfAuthenticated::class); 
 
+Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
+
+Route::post('/agent/register', [AgentController::class, 'AgentRegister'])->name('agent.register');
+
+
+
+
 
 /// Admin group Middleware
 
